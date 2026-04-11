595a94f5220916a44f04c3152ab935f0a88a965d
levi9111 <captainlevi9111@gmail.com>
2026-04-11 16:06:58 +0600

    feat: implement global error handling and 404 middleware with custom AppError class

--- template/src/app.ts
+++ template/src/app.ts
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import router from './app/routes/index';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
 
 const app: Application = express();
 
@@ -14,5 +16,8 @@ app.use('/api/v1', router);
 app.get('/', (_req: Request, res: Response) => {
   res.send('Welcome to Express Modular Server!');
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
